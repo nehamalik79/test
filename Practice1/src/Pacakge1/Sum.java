b3cb4aeff6cf99624ff363ef9db30001b40a7b71
nehamalik79 <nehamalik79@gmail.com>
2018-05-09 11:39:31 +0530

    Added Sum1

--- Practice1/src/Pacakge1/Sum.java
+++ Practice1/src/Pacakge1/Sum.java
@@ -4,8 +4,11 @@ public class Sum {
 	public static void main (String [] args)
 	{
 		int a=0,b=0,sum=0;
+		int a1=4,b1=1, sum1=0;
 		sum =a+b;
+		sum1=a1+b1;
 		System.out.println(sum + "1nnb");
+		System.out.println(sum1);
 	}
 
 }
