7ae412fc452db94f8eb9cddc75375faa97ef690d
nehamalik79 <nehamalik79@gmail.com>
2018-05-09 11:27:19 +0530

    added difference of two numbers code

--- Practice1/src/Pacakge1/Sum.java
+++ Practice1/src/Pacakge1/Sum.java
@@ -3,9 +3,12 @@ package Pacakge1;
 public class Sum {
 	public static void main (String [] args)
 	{
-		int a=0,b=0,sum=0;
+		int a=5,b=10,sum=0;
+		int c=10,d=2, diff=0;
 		sum =a+b;
-		System.out.println(sum + "1nnb");
+		diff=c-d;
+		System.out.println(sum +" "+"it is the sum");
+		System.out.println(diff);
 	}
 
 }
