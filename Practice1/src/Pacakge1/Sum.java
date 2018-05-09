04e21b15e2dd5725fb2f357e708574e9f2440a73
nehamalik79 <nehamalik79@gmail.com>
2018-05-09 11:50:22 +0530

    resolved conflicts

--- Practice1/src/Pacakge1/Sum.java
+++ Practice1/src/Pacakge1/Sum.java
@@ -3,21 +3,20 @@ package Pacakge1;
 public class Sum {
 	public static void main (String [] args)
 	{
-<<<<<<< HEAD
+
 		int a=0,b=0,sum=0;
 		int a1=4,b1=1, sum1=0;
 		sum =a+b;
 		sum1=a1+b1;
 		System.out.println(sum + "1nnb");
 		System.out.println(sum1);
-=======
-		int a=5,b=10,sum=0;
+
 		int c=10,d=2, diff=0;
 		sum =a+b;
 		diff=c-d;
 		System.out.println(sum +" "+"it is the sum");
 		System.out.println(diff +" " + "It is the difference");
->>>>>>> j1
+
 	}
 
 }
