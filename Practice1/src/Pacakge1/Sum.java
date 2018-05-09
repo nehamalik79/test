704238b87b540e2897aaf2ccddf16fb9b56d1423
nehamalik79 <nehamalik79@gmail.com>
2018-05-09 11:56:19 +0530

    removed sum variable from j1

--- Practice1/src/Pacakge1/Sum.java
+++ Practice1/src/Pacakge1/Sum.java
@@ -3,11 +3,10 @@ package Pacakge1;
 public class Sum {
 	public static void main (String [] args)
 	{
-		int a=5,b=10,sum=0;
+	
 		int c=10,d=2, diff=0;
-		sum =a+b;
 		diff=c-d;
-		System.out.println(sum +" "+"it is the sum");
+		//System.out.println(sum +" "+"it is the sum");
 		System.out.println(diff +" " + "It is the difference");
 	}
 
